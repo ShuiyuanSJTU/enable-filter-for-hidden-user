e2f6e9824920a8153ef37eb95dca9778826ea937
Banson Xu <xzcxzcyy@126.com>
2024-10-23 15:06:31 +0800

    feat: add fix for filtered-notice widget, so that it does not crash on single topic post.

---
+++ javascripts/discourse/api-initializers/filtered-notice-fix.js
@@ -0,0 +1,45 @@
+// /javascripts/discourse/initializers/custom-filtered-notice.js
+import { h } from "virtual-dom";
+import { apiInitializer } from "discourse/lib/api";
+import { avatarFor } from "discourse/widgets/post";
+import I18n from "I18n";
+
+export default apiInitializer("1.8.0", (api) => {
+  api.reopenWidget("posts-filtered-notice", {
+    html(attrs) {
+      const filters = attrs.streamFilters;
+      if (filters.username_filters) {
+        const filteredPostsCount = parseInt(attrs.filteredPostsCount, 10);
+        let firstUserPost, userPostsCount;
+        userPostsCount = filteredPostsCount;
+        if (filteredPostsCount < 2) {
+          firstUserPost = attrs.posts[0];
+        } else {
+          firstUserPost = attrs.posts[1];
+          if (attrs.posts[1].user_id !== attrs.posts[0].user_id) {
+            userPostsCount = filteredPostsCount - 1;
+          }
+        }
+        return [
+          h(
+            "span.filtered-replies-viewing",
+            I18n.t("post.filtered_replies.viewing_posts_by", {
+              post_count: userPostsCount,
+            })
+          ),
+          h(
+            "span.filtered-avatar",
+            avatarFor.call(this, "small", {
+              template: firstUserPost.avatar_template,
+              username: firstUserPost.username,
+              url: firstUserPost.usernameUrl,
+            })
+          ),
+          this.attach("poster-name", firstUserPost),
+          this.attach("filter-show-all", attrs),
+        ];
+      }
+      return this._super(attrs);
+    },
+  });
+});
